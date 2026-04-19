d92cd48cc8a27599edeec771ec0d7fedc480e0e9
veselvv <veselvv12@yandex.ru>
2026-04-19 16:12:42 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker run -d -v postgres_data:/var/lib/postgresql/data -e POSTGRES_PASSWORD=123 -e POSTGRES_DB=users -e POSTGRES_USER=postgres \
+docker run -d -v postgres_data:/var/lib/postgresql/data -e POSTGRES_PASSWORD=123 -e POSTGRES_DB=tasks -e POSTGRES_USER=postgres \
 --name postgres_db -p 5433:5432 postgres:15
 sleep 3
 cd ./build
